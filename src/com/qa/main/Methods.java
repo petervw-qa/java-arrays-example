52e17e4aa993e1f393dff14cd9fd45925c0d6829
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-08 13:53:52 +0000

    from the arrays community page

---
+++ src/com/qa/main/Methods.java
@@ -0,0 +1,23 @@
+package com.qa.main;
+
+public class Methods {
+	
+	int[] FirstArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	int[] SecondArray = {1, 2, 3, 4, 5};
+	
+	public void theSecondExercise() {
+		
+		for (int i = 1; i<= 10; i++) {
+			
+			System.out.println(i);
+			
+			for (int j = 1; j <= 1; j++) {
+				
+				System.out.println(i*10);
+			}
+		}
+	}
+	
+
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,20 @@
+package com.qa.main;
+
+public class Runner {
+	public static void main(String[] args) {
+		
+		Methods fa = new Methods();
+		System.out.println(fa.FirstArray[0]);
+		System.out.println(fa.FirstArray[1]);
+		System.out.println(fa.FirstArray[2]);
+		System.out.println(fa.FirstArray[3]);
+		System.out.println(fa.FirstArray[4]);
+		System.out.println(fa.FirstArray[5]);
+		System.out.println(fa.FirstArray[6]);
+		System.out.println(fa.FirstArray[7]);
+		System.out.println(fa.FirstArray[8]);
+		System.out.println(fa.FirstArray[9]);
+		fa.theSecondExercise();
+	}
+
+}
